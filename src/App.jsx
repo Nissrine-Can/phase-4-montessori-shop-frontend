facbe0a50fe8367a8cdd0dbbb68eeeeab23063c8
Nissrine Canina <niss.canina@gmail.com>
2022-04-11 10:05:57 -0400

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navigation/Navbar'
+import Home from './components/static/Home'
+import Signup from './components/authentication/Signup'
+import Login from './components/authentication/Login'
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </Router>
   )
 }
 
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { Grid, Menu } from 'semantic-ui-react'
+
 
 const Navbar = () => {
+
+    const linkStyles = {
+        width: "5 rem",
+        padding: "12px",
+        margin: "0 6px 6px",
+        textDecoration: "none",
+        color: "black",
+      }
+
+
   return (
-    <div>Navbar</div>
+    <div>
+      <Grid padded>
+       <Menu borderless fluid size="large">
+       
+            <Link to="/">
+                <Menu.Item>
+                MONTESSORI SHOP
+                </Menu.Item>
+            </Link>
+        
+            <NavLink 
+                    to="/"
+                    style={linkStyles}
+                    >
+                        <Menu.Item>Home</Menu.Item>
+            </NavLink>
+       
+        <NavLink 
+                to="/about-us"
+                style={linkStyles}
+                activeStyle={{ background: "darkblue" }}
+                >
+                    <Menu.Item>About us</Menu.Item>
+                </NavLink>
+
+         <NavLink 
+                to="/items"
+                style={linkStyles}
+                activeStyle={{ background: "darkblue" }}
+                >
+                    <Menu.Item>Explore</Menu.Item>
+             </NavLink>
+         
+         <NavLink 
+                to="/favorites"
+                style={linkStyles}
+                activeStyle={{ background: "darkblue" }}
+                >
+               <Menu.Item>Favorites</Menu.Item> 
+        </NavLink>
+
+         <Menu.Menu position='right'>
+
+         <NavLink 
+                to="/signup"
+                style={linkStyles}
+                activeStyle={{ background: "darkblue" }}
+             >
+                <Menu.Item>Sign Up</Menu.Item>
+         </NavLink>
+         
+         <NavLink 
+                to="/login"
+                style={linkStyles}
+                activeStyle={{ background: "darkblue" }}
+             >
+          <Menu.Item>Login</Menu.Item>
+          </NavLink>
+
+        </Menu.Menu>
+         
+        
+        </Menu>
+        </Grid>
+        
+    </div>
   )
 }
 
