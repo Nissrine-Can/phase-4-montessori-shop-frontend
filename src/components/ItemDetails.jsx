157982410bb3d8030f97189657c71609227376b5
Nissrine Canina <niss.canina@gmail.com>
2022-05-24 09:57:19 -0400

    add favorite feature

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { Grid, Image, Button, Icon } from 'semantic-ui-react'
+import BuyNowModal from './BuyNowModal'
 
-import { Grid, Image, Button } from 'semantic-ui-react'
-import AddToCartModal from './AddToCartModal'
 
+const ItemDetails = ({ 
+      onAddToCart, 
+      selectedItem,
+      setSelectedItem,
+      backToItems,
+      cartItems, 
+      addFavorite,
+      removeFavorite,
+      currentUser
+      }
+      ) => {
+        
+        const {id} = useParams()
+        
 
-const ItemDetails = ({ onAddToCart, selectedItem, backToItems }) => {
- 
-    
-  return (
+        useEffect(() => {
+          fetch(`/items/${id}`)
+              .then(resp => resp.json())
+              .then((selectedItem) => {
+                setSelectedItem(selectedItem)
+              
+              })
+        }, [id, setSelectedItem])
+
+      //   const favorOrUnfavorIcon = (selectedItem) => {
+
+      //     const favId = currentUser.favorite_items.find(f => f.item_id === selectedItem.id)
+      //     if (favId) {
+      //       return <Icon onClick={() => removeFavorite(selectedItem.id) } color="red" name="heart" />
+      //    } else {
+      //      return <Icon onClick={() => addFavorite(selectedItem.id) } color="red" name="heart outline" />
+      //   }
+      //  }       
+      
+ return (
     <div>
-        <Grid>
+    {selectedItem ? 
+    <Grid>
           <Grid.Column mobile={16} computer={4}>
               <Image src={selectedItem.image} alt={selectedItem.name} />
               <br />
+              <div >
+                <Icon onClick={() => addFavorite(selectedItem) } color="red" name="heart outline" />
+              </div>
           </Grid.Column>
           <Grid.Column mobile={16} computer={9}>
              <h1>{selectedItem.name}</h1>
@@ -23,9 +58,21 @@ const ItemDetails = ({ onAddToCart, selectedItem, backToItems }) => {
              <Button onClick={backToItems} >Back To All Items</Button>
             <br />
             <br />
-             <AddToCartModal onAddToCart={onAddToCart} />
+             <BuyNowModal 
+                cartItems={cartItems} 
+                onAddToCart={onAddToCart} 
+                selectedItem={selectedItem}
+                  />
+                  <br />
+                  <br />
+                  <br />
+                  <div>
+                   
+                </div>
         </Grid.Column>
         </Grid>
+     : <h2>Loading...</h2>}
+        
     </div>
   )
 }
