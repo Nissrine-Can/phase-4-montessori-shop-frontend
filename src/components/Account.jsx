36c978c35916e707c4f2105db094a98be8b5f04f
Nissrine Canina <niss.canina@gmail.com>
2022-05-24 09:53:23 -0400

    add user account component

---
+++ src/components/Account.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
+
+
+const Account = () => {
+
+ const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate("/listings")
+    }
+    const handleClickPurchased = () => {
+      navigate("/purchased_items")
+    }
+
+  return (
+    <>
+    <div>
+        <Button color="orange" onClick={handleClick}>Listings</Button>
+    </div>
+    <br />
+    <br />
+    <div>
+        <Button   color="olive" onClick={handleClickPurchased}>Purchased Items</Button>
+    </div>
+
+    </>
+
+  )
+}
+
+export default Account
