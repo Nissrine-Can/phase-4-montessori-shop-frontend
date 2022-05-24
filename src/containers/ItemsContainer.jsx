ab98fef9b740598e298ef58810313c572bfc9c56
Nissrine Canina <niss.canina@gmail.com>
2022-05-24 10:10:54 -0400

    update item container

--- src/containers/ItemsContainer.jsx
+++ src/containers/ItemsContainer.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Container, Card } from 'semantic-ui-react'
+import Search from '../components/Search'
+import Filter from '../components/Filter'
 import ItemCard from '../components/ItemCard'
 
+const ItemsContainer = ({ 
+    searchItems, 
+    filterItems,
+    items,
+    addFavorite,
+    removeFavorite,
+    currentUser, 
+    
+}) => {
+ 
+    
 
-const ItemsContainer = ({ items, itemSelected }) => {
-
-  
- return (
-    <div className="items-container">
-        <Card.Group itemsPerRow={3}>
+return (
+    <div>
+        <Container>
+            <br />
+            <h2>Our Inventory</h2>
+            <br />
+            <Search searchItems={searchItems} />
+            <Filter items={items} filterItems={filterItems} />
+            <br />
+        
+            <div className="items-container">
+            <Card.Group itemsPerRow={3}>
             {items.map((item) => (
-              <ItemCard key={item.id} item={item} itemSelected={itemSelected} />
+            <ItemCard
+             key={item.id} 
+             item={item} 
+             addFavorite={addFavorite}
+             removeFavorite={removeFavorite}
+             currentUser={currentUser}
+             redHeart={false}
+             />
             ))}
         </Card.Group>
+    </div>
+            
+         </Container>
     </div>
   )
 }
