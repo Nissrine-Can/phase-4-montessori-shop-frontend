497d63a4345d17c94db5852dcdd36c13870ba499
Nissrine Canina <niss.canina@gmail.com>
2022-05-31 12:23:19 -0400

    updated baseurl

--- src/Globals.js
+++ src/Globals.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "http://localhost:3001"
-export const BASE_URL = "http://backend-montessori-shop.herokuapp.com"
+//export const BASE_URL = "https://localhost:3001"
+export const BASE_URL = "https://backend-montessori-shop.herokuapp.com"
