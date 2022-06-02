ac13cfddf60d7915e52cf7fefcf0db21d658b7ed
Nissrine Canina <niss.canina@gmail.com>
2022-06-02 19:41:08 -0400

    reverted back to proxy

--- src/Globals.js
+++ src/Globals.js
@@ -1,2 +1,3 @@
-//export const BASE_URL = "https://localhost:3001"
-export const BASE_URL = "https://backend-montessori-shop.herokuapp.com"
+//export const BASE_URL = "http://localhost:3001"
+//export const BASE_URL = "https://backend-montessori-shop.herokuapp.com"
+export const BASE_URL = ""
